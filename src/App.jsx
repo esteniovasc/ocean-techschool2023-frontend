f8aa975f6afdf8cafbed01fc2a125a59ffc768b7
esteniovasc <119541023+esteniovasc@users.noreply.github.com>
2023-09-26 00:02:48 +0000

    metade da aula 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
   return (
 	<>
-	  <div>
-		<h2>Agata Nunes e Maria José</h2>
-		<img src="https://img.quizur.com/f/img649433247be491.67576251.jpg" />
-	  </div>
+
+	<Card/>
+	<Card/>
 	</>
   )
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,18 @@
+/* compactar o codigo princial */
+
+import "./Card.css"
+
+export default function Card() {
+    
+    return <div className='card'>
+    <h2>Limão</h2>
+
+    <tag>Rico em vitamina C</tag>
+    <tag>Acidez PH7</tag>
+    <tag>Azedo</tag>
+    <p> </p>
+
+    <img src="https://static3.tcdn.com.br/img/img_prod/450860/muda_de_limao_siciliano_enxertadas_542_1_20190611093557.jpg" />
+  
+  </div>
+}
